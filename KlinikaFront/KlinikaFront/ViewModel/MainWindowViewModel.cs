5ad093d1998f2d8628bcb16114ceb86f6670d45a
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-05-09 15:29:49 +0200

    Base ViewModel template

---
+++ KlinikaFront/KlinikaFront/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlinikaFront.ViewModel
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private IPageViewModel _currentPageViewModel;
+        private List<IPageViewModel> _pageViewModels;
+
+        public List<IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                {
+                    _pageViewModels = new List<IPageViewModel>();
+                }
+                return _pageViewModels;
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                _currentPageViewModel = value;
+                OnPropertyChanged("CurrentPageViewModel");
+            }
+        }
+
+        private void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+            {
+                PageViewModels.Add(viewModel);
+            }
+            CurrentPageViewModel = PageViewModels.FirstOrDefault(vm => vm == viewModel);
+        }
+
+        private void OnGo1Screen(object obj)
+        {
+            ChangeViewModel(PageViewModels[0]);
+        }
+
+        private void OnGo2Screen(object obj)
+        {
+            ChangeViewModel(PageViewModels[1]);
+        }
+
+        public MainWindowViewModel()
+        {
+            PageViewModels.Add(new IndexViewModel());
+            PageViewModels.Add(new MainViewModel());
+
+            CurrentPageViewModel = PageViewModels[0];
+
+            Mediator.Subscribe("GoTo1Screen", OnGo1Screen);
+            Mediator.Subscribe("GoTo2Screen", OnGo2Screen);
+        }
+    }
+}
